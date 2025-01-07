dd35dc67cb44cfb9fbea0eb2530d071c23387efb
Muhammad Adil Shahzad <muhammad.shahzad@rakbank.ae>
2025-01-07 17:40:28 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,40 @@
+# Random password generation
+resource "random_password" "password" {
+  length           = 16
+  special          = false
+  override_special = true
+  min_upper        = 1
+  min_lower        = 1
+  min_numeric      = 1
+}
+
+# Secrets Manager secret definition
+resource "aws_secretsmanager_secret" "digital_wallet_secret" {
+  name        = "digital-wallet-secret"
+  description = "Secret used by digital wallet RDS database"
+  tags = {
+    Environment  = "deh-dev"
+    map-migrated = "d-server-03is5ms7k94v6w"
+  }
+}
+
+# Secrets Manager secret version to store username and password
+resource "aws_secretsmanager_secret_version" "admin_user_pass" {
+  secret_id = aws_secretsmanager_secret.digital_wallet_secret.id
+  secret_string = jsonencode({
+    username = var.rds_username  # Define username dynamically through variables
+    password = random_password.password.result
+  })
+}
+
+# Data source to fetch the secret version
+data "aws_secretsmanager_secret_version" "admin_user_pass" {
+  secret_id = aws_secretsmanager_secret.digital_wallet_secret.id
+
+  depends_on = [aws_secretsmanager_secret_version.admin_user_pass]
+}
+
+# RDS Module with dependencies
 module "digital-wallet" {
   source = "git::https://github.com/rakbank-internal/terraform-aws-rds-postgres-module.git?ref=v1.3.0"
 
@@ -23,39 +60,21 @@ module "digital-wallet" {
   create_replica             = var.create_replica
   replica_count              = var.replica_count
   force_ssl                  = var.force_ssl
-  username                   = jsondecode(data.aws_secretsmanager_secret_version.admin_user_pass.secret_string)["username"]
-  password                   = jsondecode(data.aws_secretsmanager_secret_version.admin_user_pass.secret_string)["password"]
+
+  # Username and password retrieved from Secrets Manager
+  username = jsondecode(data.aws_secretsmanager_secret_version.admin_user_pass.secret_string)["username"]
+  password = jsondecode(data.aws_secretsmanager_secret_version.admin_user_pass.secret_string)["password"]
+
+  # Explicit dependency to ensure the secret is created before RDS
+  depends_on = [
+    aws_secretsmanager_secret.digital_wallet_secret,
+    aws_secretsmanager_secret_version.admin_user_pass
+  ]
 }
 
-create secret with random password 
-
-  "deh-db-dev-01-secret" = {
-    name        = "deh-db-dev-01-secret"
-    description = "Secret used by dev rds database"
-    tags = {
-      Environment  = "deh-dev",
-      map-migrated = "d-server-03is5ms7k94v6w",
-    }
-  }
-
-resource "random_password" "password" {
-  length           = 16
-  special          = false
-  override_special = true
-  min_upper        = 1
-  min_lower        = 1
-  min_numeric      = 1
-}
-resource "aws_secretsmanager_secret_version" "admin_user_pass" {
-  secret_id = var.secret_id
-  secret_string = jsonencode({
-    username = "postgres"
-    password = random_password.password.result
-  })
-  version_stages = ["AWSCURRENT"]
-}
-data "aws_secretsmanager_secret_version" "admin_user_pass" {
-  secret_id = var.secret_id
-
-  depends_on = [aws_secretsmanager_secret_version.admin_user_pass]
+# Variables for RDS username
+variable "rds_username" {
+  description = "RDS username to be stored in Secrets Manager"
+  type        = string
+  default     = "postgres"  # You can override this as needed
 }
