624a35fdb877932a31e8b29edd17a983b665042b
Muhammad Adil Shahzad <muhammad.shahzad@rakbank.ae>
2025-01-10 15:48:04 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-1- Outputs should be enable for new RDS module.
-2- Create Secret for DB password
-3- For db_subnet_group_name, let it be same as VPC is same.
-4- db_ingress_cidr_blocks make it separate and clone values from DEH RDS, on requirement one can update
+  I think secret for storing db password should be created in the first apply.
+RDS changes should be done in the second apply. 
+Or can we add a dependency for random password with AWS secret module ?
+Both methods are fine,  try to add dependency as per above suggestion. and share PR. Secrets should be available before injecting random password string in it. 
+
 
 ### Digital Wallet(Bitpanda) - RDS ###
 
@@ -35,7 +36,7 @@ module "digital_wallet" {
 
   vpc_id                     = var.vpc_id
   db_name                    = var.dw_db_name
-  ingress_cidr_blocks        = var.db_ingress_cidr_blocks
+  ingress_cidr_blocks        = var.dw_db_ingress_cidr_blocks
   engine_version             = var.engine_version
   instance_class             = var.dw_instance_class
   family                     = var.family
@@ -65,44 +66,3 @@ module "digital_wallet" {
     aws_secretsmanager_secret_version.digital_wallet_pass
   ]
 }
-
-
-tfvars develop
-
-subnets = ["subnet-016a90028714de462", "subnet-05cb126b31e6dba8a", "subnet-01faf667f57e784a7"]
-
-
-db_ingress_cidr_blocks     = ["10.85.128.0/20", "10.85.201.8/32", "10.85.201.10/32", "10.85.184.0/23", "10.85.139.177/32", "10.85.201.54/32"]
-engine_version             = "16.3"
-family                     = "postgres16"
-major_engine_version       = "16.3"
-port                       = 5500
-multi_az                   = true
-allocated_storage          = 500
-max_allocated_storage      = 550
-storage_type               = "gp3"
-apply_immediately          = true
-deletion_protection        = true
-db_subnet_group_name       = "deh-db-dev-01-subnet-group"
-auto_minor_version_upgrade = false
-create_replica             = true
-replica_count              = 0
-
-force_ssl = "0"
-
-################################################################################
-# RDS Postgres Module Values
-################################################################################
-
-db_name        = "deh-db-dev-01"
-instance_class = "db.m5.2xlarge"
-secret_id      = "deh-db-dev-01-secret"
-
-################################################################################
-# Digital Wallet RDS Postgres Module Values
-################################################################################
-
-dw_db_name        = "da_dev_rds_db"
-dw_instance_class = "db.m5.large"
-dw_rds_username   = "postgres"
-dw_secret_id      = "digital-wallet-secret"
